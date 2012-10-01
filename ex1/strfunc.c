d89be3e98bff21fc3f6e7a2922412f5cb98f45c8
Michal Staniaszek <m.staniaszek@gmail.com>
2012-10-01 22:18:39 +0100

    string comparison seems to work ok.

--- ex1/strfunc.c
+++ ex1/strfunc.c
@@ -1,1 +1,29 @@
 #include "ex1.h"
+
+int strcomp(char*, char*);
+
+int main(int argc, char *argv[])
+{
+
+    printf("%d\n", strcomp("a", "aa"));
+    
+    return 0;
+}
+
+/* Returns the result of an alphanumeric comparison of string s1 and s2. 
+   If s1 is of greater value than the second, the function returns 1.
+   If they are equal, 0 is returned. If the second is greater, -1 is returned.
+   a has the highest value, z has the lowest. 
+   Characters are converted to lowercase before being compared.*/
+int strcomp(char* s1, char* s2)
+{
+    for(; *s1 != '\0' && *s2 != '\0'; ++s1, ++s2){
+	if (tolower(*s1) > tolower(*s2))
+	    return -1;
+	else if (tolower(*s1) < tolower(*s2))
+	    return 1;
+    }
+
+    return 0;
+    
+}
