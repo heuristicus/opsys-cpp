b3fe0f4429c6c4ad33551d4afd99fca31ae0b537
Michal Staniaszek <m.staniaszek@gmail.com>
2012-10-02 09:41:33 +0100

    very basic open, read and close operations

--- ex1/lsdir.c
+++ ex1/lsdir.c
@@ -1,1 +1,24 @@
 #include "ex1.h"
+#include <dirent.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2){
+	printf("usage: lsdir [directory]\n");
+	exit(1);
+    }
+    
+    DIR *dir_ptr;
+    struct dirent *entry;
+    
+    if ((dir_ptr = opendir(argv[1])) == NULL){
+	printf("Directory invalid.\n");
+	exit(1);
+    }
+
+    while ((entry = readdir(dir_ptr)) != NULL){
+	printf("%s\n", entry->d_name);
+    }
+    
+    return 0;
+}
