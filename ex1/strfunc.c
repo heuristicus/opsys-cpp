a0a9c723f64d64b9f0867ceb9346f03fe0dbc1c9
Michal Staniaszek <m.staniaszek@gmail.com>
2012-10-03 19:24:19 +0100

    made function shorter

--- ex1/strfunc.c
+++ ex1/strfunc.c
@@ -9,22 +9,13 @@
 */
 int strcomp(char* s1, char* s2)
 {
-    for(i = 0; *s1 != '\0' && *s2 != '\0'; ++s1, ++s2){
-	if (tolower(*s1) > tolower(*s2))
+    for(; ; ++s1, ++s2){
+	if (tolower(*s1) > tolower(*s2) || *s2 == '\0')
 	    return -1;
-	else if (tolower(*s1) < tolower(*s2))
+	else if (tolower(*s1) < tolower(*s2) || *s1 == '\0')
 	    return 1;
     }
-
-    /* If we get to here, the strings are the same up to the length
-       of the shorter string. The shorter string is higher value, so
-       return a value accordingly.
-    */
-    if (*s1 == '\0')
-	return 1;
-    else if (*s2 == '\0')
-	return -1;
-    else
-	return 0;
+    
+    return 0;
     
 }
