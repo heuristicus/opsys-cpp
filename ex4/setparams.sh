1c4fa093dd91dc4879101fcbcd72b6714325f12d
Michal Staniaszek <m.staniaszek@gmail.com>
2012-11-22 22:11:06 +0000

    bash script parses arguments and is more helpful

--- ex4/setparams.sh
+++ ex4/setparams.sh
@@ -1,27 +1,73 @@
 #!/bin/bash
 
+usage(){
+cat <<EOF
+usage $0 options
+
+Send various parameters to the proc file for the ratelimit kernel module.
+There is only minimal checking done on the parameters, so make sure you get them right!
+
+OPTIONS:
+  -h   show this message
+  -p   port to monitor
+  -l   overflow limit
+  -w   wait time after limit exceeded
+EOF
+}
+
+
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 1>&2
    exit 1
 fi
 
-if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
-    echo "This script sends parameters to the proc file /proc/ratelimit. Not all parameters are required. There is no checking done on the parameters, so make sure you get them right!"
-    echo "usage: `basename $0` portno packetlimit waittime"
+if [ "$#" -eq 0 ]; then
+    usage
     exit
 fi
 
-if [[ -n "$1" ]]; then
-    echo "Sending param 1"
-    echo "P $1" > /proc/ratelimit
+if [ ! -e "/proc/ratelimit" ]; then
+    echo "Module ratelimit is not running. Use \"insmod ratelimit.ko\" to load it."
+    exit
 fi
 
-if [[ -n "$2" ]]; then
-    echo "Sending param 2"
-    echo "L $2" > /proc/ratelimit
+PORT=
+LIMIT=
+WAIT=
+while getopts “hp:l:w:” OPTION
+do
+     case $OPTION in
+         h)
+             usage
+             exit 1
+             ;;
+         p)
+	     PORT=$OPTARG
+             ;;
+         l)
+	     LIMIT=$OPTARG
+             ;;
+	 w)
+	     WAIT=$OPTARG
+	     ;;
+         ?)
+             usage
+             exit
+             ;;
+     esac
+done
+
+if [[ -n $PORT ]]; then
+    echo "P $PORT" > /proc/ratelimit
+    echo "Sent port number"
 fi
 
-if [[ -n "$3" ]]; then
-    echo "Sending param 3"
-    echo "W $3" > /proc/ratelimit
+if [[ -n "$LIMIT" ]]; then
+    echo "L $LIMIT" > /proc/ratelimit
+    echo "Sent limit"
+fi
+
+if [[ -n "$WAIT" ]]; then
+    echo "W $WAIT" > /proc/ratelimit
+    echo "Sent wait time"
 fi
